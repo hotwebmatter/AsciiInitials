3aa16e47411692a9768715d7f2f0a46023b165e8
Matt Obert on Windows <matt@hotwebmatter.com>
2019-09-02 14:01:31 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AsciiInitials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // output
+            WriteLine();
+            WriteLine("M     M      OOO");
+            WriteLine("MM   MM     O   O");
+            WriteLine("M M M M    O     O");
+            WriteLine("M  M  M    O     O");
+            WriteLine("M     M    O     O");
+            WriteLine("M     M     O   O");
+            WriteLine("M     M      OOO");
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
